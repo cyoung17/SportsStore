14f2c0261ada8c7ffbe8918a9533b66d441d6031
youngc <youngc@816ZXZ1>
2017-01-16 15:58:59 -0500

    Add project files.

---
+++ SportsStore.Domain/Repository/ProductionRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Repository
+{
+    public class Production : IProductRepository
+    {
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return new List<Product>
+                {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Baseball", Price = 35 },
+                    new Product { Name = "Soccer", Price = 45 }
+                };
+            }
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using SportsStore.Domain.Repository;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController()
+        {
+            this.repository = new ProductRepository();
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            return View(repository.Products
+                        .OrderBy(p => p.ProductID)
+                        .Skip((page -1) * PageSize)
+                        .Take(PageSize));
+        }
+    }
+}
